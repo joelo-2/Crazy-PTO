2cfd90f775ee01f5435718e0a45f0bae42e78821
joelo-2 <joel.oine2@gmail.com>
2025-08-27 17:12:06 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,3 +4,11 @@ const nextConfig = {
   experimental: { appDir: true },
 };
 module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  experimental: { appDir: true },
+  typescript: { ignoreBuildErrors: true }, // optional: allow build even if TS complains
+  eslint: { ignoreDuringBuilds: true }     // optional: skip ESLint in CI
+};
+module.exports = nextConfig;
